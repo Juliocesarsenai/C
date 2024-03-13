7b58b5fa596d3dd6a6bbbd665fdb0f823b27a06d
Juliocesarsenai <159816707+Juliocesarsenai@users.noreply.github.com>
2024-03-13 08:53:22 -0300

    Add files via upload

---
+++ Atividade9.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <locale.h>
+
+
+int main(){
+	setlocale(LC_ALL,"");
+	//Declaração de Variaveis
+	float notas,soma=0,media;
+	int i=0;
+	char resposta;
+	
+	
+	
+	//Solicitando dados do usuario
+
+	
+	do{
+
+	
+	printf("Insira uma nota:\n");
+	scanf("%f",&notas);
+	setbuf(stdin,0);
+	printf("Deseja inserir mais uma nota? S/N \n");
+	scanf("%c",&resposta);
+
+	
+	i=i+1;
+	soma=soma + notas;
+	}while(resposta=='S');
+	
+	//Operações aritmeticas
+
+	media=soma/i;
+
+printf("Media: %.1f. \n",media);
+printf("Numero de repetições: %d \n",i);
+}
+
+
+
+
+
